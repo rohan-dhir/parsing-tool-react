dd41359c326e96079e5928eb697b26d5d6b90c27
Rohan Dhir <rohan.dhir@outlook.com>
2023-01-26 22:36:49 -0500

    Modified Variables

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -5,7 +5,10 @@ import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider } from '@ap
 //For local development
 //const { CLIENT_URI } = require('./config.json');
 
-const CLIENT_URI = "https://parsing-tool-react.herokuapp.com/";
+const dotenv = require('dotenv');
+dotenv.config();
+
+const CLIENT_URI = process.env.CLIENT_URI
 
 const httpLink = createHttpLink ({
     uri: CLIENT_URI
